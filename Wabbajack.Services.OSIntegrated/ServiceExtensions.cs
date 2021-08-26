0e7ddfd014eabf78ee2b7a8c53df5c344a4c2a38
Timothy Baldridge (halgari) <tbaldridge@gmail.com>
2021-08-25 18:32:48 -0600

    Make a custom folder for each TemporaryFileManager

--- Wabbajack.Services.OSIntegrated/ServiceExtensions.cs
+++ Wabbajack.Services.OSIntegrated/ServiceExtensions.cs
@@ -38,7 +38,7 @@ namespace Wabbajack.Services.OSIntegrated
             var options = new OSIntegratedOptions();
             cfn?.Invoke(options);
 
-            service.AddTransient(s => new TemporaryFileManager(KnownFolders.EntryPoint.Combine("temp")));
+            service.AddTransient(s => new TemporaryFileManager(KnownFolders.EntryPoint.Combine("temp", Guid.NewGuid().ToString())));
             
             service.AddSingleton(s => options.UseLocalCache ? 
                 new FileHashCache(s.GetService<TemporaryFileManager>()!.CreateFile().Path) 
