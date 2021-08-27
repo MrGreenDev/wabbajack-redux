ef565a8d78b7d8fa9eae9d8d1492f0f314bd593f
Timothy Baldridge (halgari) <tbaldridge@gmail.com>
2021-08-26 18:45:04 -0600

    fix known folders

--- Wabbajack.Paths.IO/KnownFolders.cs
+++ Wabbajack.Paths.IO/KnownFolders.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Reflection;
 
 namespace Wabbajack.Paths.IO
 {
     public static class KnownFolders
     {
-        public static AbsolutePath EntryPoint => Environment.ProcessPath!.ToAbsolutePath().Parent;
+        public static AbsolutePath EntryPoint => Assembly.GetExecutingAssembly().Location.ToAbsolutePath().Parent;
 
         public static AbsolutePath AppDataLocal =>
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData).ToAbsolutePath();
