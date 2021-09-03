9084c24717ac13d6e28766fd73043db6c51f19b5
Timothy Baldridge <tbaldrid@cisco.com>
2021-09-02 18:37:24 -0600

    add navigation to MainWindowViewModel

--- Wabbajack.App/ViewModels/MainWindowViewModel.cs
+++ Wabbajack.App/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Avalonia.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
@@ -23,24 +25,37 @@ namespace Wabbajack.App.ViewModels
         public Control CurrentScreen { get; set; }
         
         [Reactive]
+        private ImmutableStack<Control> BreadCrumbs { get; set; } = ImmutableStack<Control>.Empty; 
+
+        [Reactive]
         public ReactiveCommand<Unit, Unit> BackButton { get; set; }
         
         public MainWindowViewModel(IEnumerable<IScreenView> screens, IServiceProvider provider)
         {
             _provider = provider;
             _screens = screens;
+            
             Activator = new ViewModelActivator();
             this.WhenActivated(disposables =>
             {
-                BackButton = ReactiveCommand.Create(() => {}).DisposeWith(disposables);
+                BackButton = ReactiveCommand.Create(() =>
+                        {
+                            CurrentScreen = BreadCrumbs.Peek();
+                            BreadCrumbs = BreadCrumbs.Pop();
+                        },
+                        this.ObservableForProperty(vm => vm.BreadCrumbs)
+                            .Select(bc => bc.Value.Count() > 1))
+                    .DisposeWith(disposables);
             });
             
-            Receive(new NavigateTo(typeof(VectorPlexusOAuthLoginViewModel)));
+            Receive(new NavigateTo(typeof(ModeSelectionViewModel)));
 
         }
         public ViewModelActivator Activator { get; }
         public void Receive(NavigateTo val)
         {
+            BreadCrumbs = BreadCrumbs.Push(CurrentScreen);
+
             if (val.ViewModel.IsAssignableTo(typeof(GuidedWebViewModel)))
             {
                 CurrentScreen = new GuidedWebView() { ViewModel = (GuidedWebViewModel)_provider.GetService(val.ViewModel)! };
